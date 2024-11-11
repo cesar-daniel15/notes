f0fa0a4def63e150c956b1f792b65130c520ac91
cesar-daniel15 <cesardaniel.contacto@gmail.com>
2024-11-11 14:38:24 +0000

    First Commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { NoteModalComponent } from '../note-modal/note-modal.component';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  
+ // Array de tarefas
+tasks: string[] = [
+  'Levar o carro à inspeção',
+  'Fazer compras no supermercado',
+  'Estudar para o exame',
+  'Limpar a casa',
+];
 
-  constructor() {}
+constructor(private modalCtrl: ModalController) {}
 
-}
+    async openNoteModal(task: String){
+      const modal = await this.modalCtrl.create({
+        component: NoteModalComponent,
+      });
+      modal.present();
+  
+      const { data, role } = await modal.onWillDismiss();
+  
+      if (role === 'confirm') {
+       // this.message = `Hello, ${data}!`;
+      }
+    }
+  }
+
+
---
+++ src/app/note-modal/note-modal.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-note-modal',
+  templateUrl: './note-modal.component.html',
+  styleUrls: ['./note-modal.component.scss'],
+})
+export class NoteModalComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
