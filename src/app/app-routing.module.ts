0ad6ad0f3a09c478a09073ffca53d9f4ba69a9aa
cesar-daniel15 <cesardaniel.contacto@gmail.com>
2024-11-17 23:50:43 +0000

    Dellete some files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
     path: '',
-    loadChildren: () => import('./intro/intro.module').then(m => m.IntroPageModule) // Define IntroPage como inicial
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule) 
   }
 ];
 
